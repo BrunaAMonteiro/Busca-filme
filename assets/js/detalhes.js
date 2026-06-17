cdb91e579c44028be740c4ac40390d5a1cbb0312
BrunaAMonteiro <brunaaraujom31@gmail.com>
2026-06-17 17:27:16 -0300

    Adicionando novos elementos na page detalhes
    Adicionando novos elementos na page detalhes, como: poster, nota, descrição, nome, genero...

--- assets/js/detalhes.js
+++ assets/js/detalhes.js
@@ -2,12 +2,38 @@
 const midiaId = localStorage.getItem("midiaId"); // id é salvo
 const midiaTipo = localStorage.getItem("midiaTipo"); // filme ou tv
 
-    function fetchDetails() {
-        const url = `https://api.themoviedb.org/3/${midiaTipo}/${midiaId}?api_key=92ef8d0e552aa54022ede7c7b1244305&language=pt-BR`;
+function fetchDetails() {
+    const url = `https://api.themoviedb.org/3/${midiaTipo}/${midiaId}?api_key=92ef8d0e552aa54022ede7c7b1244305&language=pt-BR`;
 
-        fetch(url)
-            .then(res => res.json())
-            .then(data => console.log(data))
-    }
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
 
-    fetchDetails();
+            const details = document.getElementById("details");
+
+            const nome = data.title || data.name;
+            const generos = data.genres.map(g => g.name).join(", ");
+            const descricao = data.overview || "Sem descrição disponível.";
+            const nota = data.vote_average ? data.vote_average.toFixed(1) : "Nota indisponível";
+            const poster = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+
+            details.innerHTML = `
+                <div class="details-container">
+                    <div class="left">
+                    <img src="${poster}" alt="${nome} poster">
+                    <h2>${nome}</h2>
+                    <p class="generos">${generos}</p>
+                </div>
+                <div class="right">
+                    <div class="nota-box"> ${nota}</div>
+                    <div class="descricao-box">
+                        <span class="label"> Descrição</span>
+                        <p class="descricao">${descricao}</p>
+                        </div>
+                    </div>
+                </div>
+                `;
+        })
+}
+
+fetchDetails();
