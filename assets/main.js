0d70990c72065e51c499d85d8c3152707d191e98
BrunaAMonteiro <brunaaraujom31@gmail.com>
2026-06-16 21:45:13 -0300

    att main.js
    separei a function de busca (fetch) e add o evento de click e keypress no botão

--- assets/main.js
+++ assets/main.js
@@ -3,9 +3,7 @@ const input = document.getElementById("searchInput");
 const results = document.getElementById("results");
 
 
-bttn.addEventListener("click", function () {
-
-
+function buscar(){
     fetch(`https://api.themoviedb.org/3/search/multi?api_key=92ef8d0e552aa54022ede7c7b1244305&query=${input.value}&language=pt-BR`)
         .then(res => res.json())
         .then(data => {
@@ -29,4 +27,12 @@ bttn.addEventListener("click", function () {
             });
 
         })
+}
+
+bttn.addEventListener("click", buscar); 
+
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        buscar();
+    }
 });
